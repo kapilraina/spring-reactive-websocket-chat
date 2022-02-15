67c60f014ef718a46d42d2633de7de76d4eedde8
kapil raina <kapil.raina@gmail.com>
2022-02-15 12:54:13 +0530

    organized Imports

--- src/main/java/com/spring/springwebsockets/SpringreactivewebsocketchatApplication.java
+++ src/main/java/com/spring/springwebsockets/SpringreactivewebsocketchatApplication.java
@@ -1,23 +1,15 @@
 package com.spring.springwebsockets;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.spring.springwebsockets.sock.SimpleSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.channel.FluxMessageChannel;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.publisher.Sinks;
-import reactor.core.publisher.Sinks.Many;
 
 @SpringBootApplication
 @RestController
@@ -31,11 +23,9 @@ public class SpringreactivewebsocketchatApplication {
   }
 
   @Bean
-  ApplicationRunner ar(@Qualifier("subfmc") FluxMessageChannel fmcsub)
-  {
-    return args ->{
+  ApplicationRunner ar(@Qualifier("subfmc") FluxMessageChannel fmcsub) {
+    return args -> {
       fmcsub.subscribe(new SimpleSubscriber());
     };
   }
-
 }
--- src/main/java/com/spring/springwebsockets/integration/IntegrationStreamFunctions.java
+++ src/main/java/com/spring/springwebsockets/integration/IntegrationStreamFunctions.java
@@ -1,45 +1,46 @@
 package com.spring.springwebsockets.integration;
 
-import com.spring.springwebsockets.model.ChatMessage;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.FluxMessageChannel;
-import org.springframework.integration.config.EnableIntegration;
 import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
-@Profile({"amqp","fuse","kafka"})
+@Profile({ "amqp", "kafka" })
 @Configuration
 public class IntegrationStreamFunctions {
-    @Autowired
-    @Qualifier("pubfmc")
-    FluxMessageChannel pubfmc;
 
-    @Autowired
-    @Qualifier("subfmc")
-    FluxMessageChannel subfmc;
+  @Autowired
+  @Qualifier("pubfmc")
+  FluxMessageChannel pubfmc;
 
-    @Bean
-    public Supplier<Flux<Message<?>>> globalchatpubchannel() {
-        return () -> {
-            return Flux.from(pubfmc);
-        };
-    }
+  @Autowired
+  @Qualifier("subfmc")
+  FluxMessageChannel subfmc;
 
-    @Bean
-    public Consumer<Flux<Message<?>>> globalchatsubchannel() {
-        return cFlux -> {
+  @Bean
+  public Supplier<Flux<Message<?>>> globalchatpubchannel() {
+    return () -> {
+      return Flux.from(pubfmc);
+    };
+  }
 
-            cFlux.log().subscribe(m -> {
-                subfmc.send(m);
-            });
-            // rabbitsubfmc.subscribeTo(cFlux);
-        };
-    }
+  @Bean
+  public Consumer<Flux<Message<?>>> globalchatsubchannel() {
+    return cFlux -> {
+      cFlux
+        .log()
+        .subscribe(
+          m -> {
+            subfmc.send(m);
+          }
+        );
+      // rabbitsubfmc.subscribeTo(cFlux);
+    };
+  }
 }
